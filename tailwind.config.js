7171a40c78c0dfb819f0a469f80993ef88debeed
Sebastian Sowizdrzal <sowizdrzal.sebastian.kontakt@gmail.com>
2021-11-24 16:57:35 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': 'Roboto, Arial, sans-serif',
+      'serif': 'Merriweather, Georgia, serif'
+    },
+    extend: {
+      colors: {
+        'primary': '#672E9B',
+        gray: {
+          300: '#f0f0f0',
+        }
+      }
+    },
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
 }
