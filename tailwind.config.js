819eca0d086d315d6800d1d440c140c4ca4b025d
Sebastian Sowizdrzal <sowizdrzal.sebastian.kontakt@gmail.com>
2021-11-26 21:14:33 +0100

    fix4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
