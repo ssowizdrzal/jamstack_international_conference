0b78a005f7bb62d22379998f9f818d74a6aa1b4f
Sebastian Sowizdrzal <sowizdrzal.sebastian.kontakt@gmail.com>
2022-01-28 14:35:26 +0100

    editor path added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,57 @@
-const path = require('path')
- 
- exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
- 
-    return new Promise((resolve, reject) => {
-        const storyblokEntry = path.resolve('src/templates/page.js')
-    
-        resolve(
-          graphql(
-            `{
-              stories: allStoryblokEntry(filter: {field_component: {eq: "page"}}) {
-                edges {
-                  node {
-                    id
-                    name
-                    slug
-                    field_component
-                    full_slug
-                    content
-                  }
+const path = require("path")
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    createPage({
+      path: `/editor`,
+      component: path.resolve("src/pages/editor.js"),
+    })
+
+    const storyblokEntry = path.resolve("src/templates/page.js")
+
+    resolve(
+      graphql(
+        `
+          {
+            stories: allStoryblokEntry(
+              filter: { field_component: { eq: "page" } }
+            ) {
+              edges {
+                node {
+                  id
+                  name
+                  slug
+                  field_component
+                  full_slug
+                  content
                 }
               }
-            }`
-          ).then(result => {
-            if (result.errors) {
-              console.log(result.errors)
-              reject(result.errors)
             }
-    
-            const entries = result.data.stories.edges
-    
-            entries.forEach((entry) => {
-                if(entry.slug !== "home") {
-                    const page = {
-                        path: `/${entry.node.full_slug}`,
-                        component: storyblokEntry,
-                        context: {
-                            story: entry.node
-                        }
-                    }
-                    createPage(page)
-                }
-            })
-          })
-        )
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        const entries = result.data.stories.edges
+
+        entries.forEach(entry => {
+          if (entry.slug !== "home") {
+            const page = {
+              path: `/${entry.node.full_slug}`,
+              component: storyblokEntry,
+              context: {
+                story: entry.node,
+              },
+            }
+            createPage(page)
+          }
+        })
       })
- }
+    )
+  })
+}
